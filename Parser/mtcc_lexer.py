fc355bac2293995ed84af2615ee3fa64c1384439
דור שפירא <Dor.Shapira@outlook.com>
2023-04-25 19:05:07 +0300

    added peek_string_literal

--- Parser/mtcc_lexer.py
+++ Parser/mtcc_lexer.py
@@ -96,6 +96,99 @@ class Lexer:
         else:
             return tk.Token(tk.TokenKind.FLOAT_LITERAL, index_, str_)
 
+    def peek_string_literal(self):
+        index_: int = self.index
+        str_: str = self.current_char
+        opener: str = self.current_char
+
+        self.peek_char()  # peek first char
+
+        while True:
+            if self.is_char(opener):
+                str_ += self.current_char
+                self.peek_char()  # peek "opener" char
+                break
+
+            if self.is_char(END_OF_FILE):
+                raise SyntaxError(f"An string/char literal ender in needed, file index: {self.index}")
+
+            if self.is_char('\\'):  # escape sequences
+                str_ += self.current_char
+                self.peek_char()  # peek / char
+
+                if self.is_char(END_OF_FILE):
+                    raise SyntaxError(f"An string/char literal ender in needed, file index: {self.index}")
+
+                if self.is_char('\''):  # single quote
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += self.current_char
+                    self.peek_char()  # peek ' char
+
+                elif self.is_char('\"'):  # double  quote
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += self.current_char
+                    self.peek_char()  # peek " char
+
+                elif self.is_char('?'):  # question mark
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += self.current_char
+                    self.peek_char()  # peek ? char
+
+                elif self.is_char('\\'):  # backslash
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += self.current_char
+                    self.peek_char()  # peek \ char
+
+                elif self.is_char('a'):  # alert (bell) character
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += '\a'
+                    self.peek_char()  # peek a char
+
+                elif self.is_char('b'):  # backspace
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += '\b'
+                    self.peek_char()  # peek b char
+
+                elif self.is_char('f'):  # form feed
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += '\f'
+                    self.peek_char()  # peek f char
+
+                elif self.is_char('n'):  # newline (line feed)
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += '\n'
+                    self.peek_char()  # peek n char
+
+                elif self.is_char('r'):  # carriage return
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += '\r'
+                    self.peek_char()  # peek r char
+
+                elif self.is_char('t'):  # horizontal tab
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += '\t'
+                    self.peek_char()  # peek t char
+
+                elif self.is_char('v'):  # vertical tab
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += '\v'
+                    self.peek_char()  # peek v char
+
+                elif self.is_char('0'):  # null character
+                    str_ = str_[0:-1]  # remove the \ char
+                    str_ += '\0'
+                    self.peek_char()  # peek 0 char
+
+                elif self.is_char('x'):  # hexadecimal representation of a character
+                    # TODO: Implement escape sequences of hexadecimal representation of a character
+                    assert False, "Not implemented"
+                continue
+
+            str_ += self.current_char
+            self.peek_char()  # peek char
+
+        return tk.Token(tk.TokenKind.STRING_LITERAL, index_, str_)
+
     def peek_operator_or_separator(self) -> tk.Token:
         index_: int = self.index
         str_: str = self.current_char
@@ -124,6 +217,9 @@ class Lexer:
                 self.tokens.append(token)
             elif self.is_char_numeric():
                 token: tk.Token = self.peek_number()
+                self.tokens.append(token)
+            elif self.is_char('\'\"'):
+                token: tk.Token = self.peek_string_literal()
                 self.tokens.append(token)
             elif self.is_char_operator_or_separator():
                 token: tk.Token = self.peek_operator_or_separator()

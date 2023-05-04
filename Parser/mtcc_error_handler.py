72b46856dd6b273013fa551f329dceb376844772
דור שפירא <Dor.Shapira@outlook.com>
2023-05-04 21:59:42 +0300

    added SpecifierQualifierListNotFound

--- Parser/mtcc_error_handler.py
+++ Parser/mtcc_error_handler.py
@@ -4,6 +4,12 @@ class PrimaryExpressionNotFound(Exception):
         super().__init__(self.message)
 
 
+class SpecifierQualifierListNotFound(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+
 class TokenExpected(Exception):
     def __init__(self, message):
         self.message = message

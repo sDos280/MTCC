78fedfce6bbb1c953a82bf7990c862031e6041f8
דור שפירא <Dor.Shapira@outlook.com>
2023-05-09 22:27:24 +0300

    added fatal_token

--- Parser/mtcc_parser.py
+++ Parser/mtcc_parser.py
@@ -70,15 +70,18 @@ class Parser:
         start_of_line = self.source_string.rfind('\n', 0, index) + 1 if '\n' in self.source_string[:index] else 0
         return self.source_string[start_of_line:index]
 
+    def fatal_token(self, token_location: int, error_string: str, raise_exception) -> None:
+        line_string: str = self.get_line_string(self.tokens[token_location].line)
+        sub_line_string: str = self.get_line_substring_at_index(self.tokens[token_location].start)  # the sub line right up to the token start
+        full_error_string: str = f"\nMTCC:{self.tokens[token_location].line + 1}:{len(sub_line_string) + 1}: "
+        full_error_string += error_string + '\n'
+        full_error_string += f"    | {line_string}\n"
+        full_error_string += f"    | {len(sub_line_string) * ' '}^{(len(self.tokens[token_location].string) - 1) * '~'}"
+        raise raise_exception(full_error_string)
+
     def expect_token_kind(self, kind: list[tk.TokenKind] | tk.TokenKind, error_string: str, raise_exception) -> None:
         if not self.is_token_kind(kind):
-            line_string: str = self.get_line_string(self.current_token.line)
-            sub_line_string: str = self.get_line_substring_at_index(self.current_token.start)  # the sub line right up to the token start
-            full_error_string: str = f"\nMTCC:{self.current_token.line + 1}:{len(sub_line_string) + 1}: "
-            full_error_string += error_string + '\n'
-            full_error_string += f"    | {line_string}\n"
-            full_error_string += f"    | {len(sub_line_string) * ' '}^{(len(self.current_token.string) - 1) * '~'}"
-            raise raise_exception(full_error_string)
+            self.fatal_token(self.index, error_string, raise_exception)
 
     def is_variable_in_block(self, variable: Variable) -> bool:
         if self.current_block is None:  # we are on the top level block

91cd1f6c92b4dd3dd18c10efa44c8f49e220796f
דור שפירא <Dor.Shapira@outlook.com>
2023-06-18 18:05:01 +0300

    small fix to example

--- tests/statement.c
+++ tests/statement.c
@@ -1,4 +1,4 @@
-for (i = 0; i < 5; i += 1)
+ for (i = 0; i < 5; i += 1)
 {
 	i -= 1;
 }
@@ -7,14 +7,32 @@ do {
 	i += 1;
 } while (i < 5);
 
-while (i < 5){
-	i += 88888.522;
-}
-
 switch (choice) {
 	case 1:
         break;
 
 	case 2:
 		break;
+}
+
+{
+    int number, i;
+    unsigned long long factorial = 1;
+
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+
+    // Error handling for negative numbers
+    if (number < 0) {
+        printf("Error: Factorial of a negative number is undefined.\n");
+        return 1;
+    }
+
+    for (i = 1; i <= number; i++) {
+        factorial *= i;
+    }
+
+    printf("The factorial of %d is %llu.\n", number, factorial);
+
+    return 0;
 }
